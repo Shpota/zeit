0f54d5a5039aff8061bf1ab66477f610d214a19a
Sasha Shpota <sashashpota@gmail.com>
2019-12-19 21:57:35 +0200

    Fix date presentation

--- app/test/date.spec.ts
+++ app/test/date.spec.ts
@@ -2,10 +2,10 @@ import {Date} from "../date";
 
 test('should convert November 7 to words', () =>
     expect(new Date(7, 10, 4).inWords())
-        .toBe('Donnerstag, den 7 November')
+        .toBe('Donnerstag, der 7. November')
 );
 
 test('should convert to words when week starts from Monday', () =>
     expect(new Date(10, 10, 7).inWords())
-        .toBe('Sonntag, den 10 November')
+        .toBe('Sonntag, der 10. November')
 );
